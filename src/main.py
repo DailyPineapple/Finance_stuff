8b3c5b46cb558e085e426b2dfb8e959440191773
Ramses <ramses@otainsight.com>
2023-09-21 10:30:07 +0200

    Revert "Revert "started using typer CLI""
    This reverts commit f5bc2201367a710b60b02cbf12fb7dee28b34617.

--- src/main.py
+++ src/main.py
@@ -1,28 +1,14 @@
-from db import Database
+import typer
+from src.db import Database
 
 database = Database()
-database.create_tables()
+app = typer.Typer()
 
-while True:
-    print("Enter 1 to add a new expense")
-    print("Enter 2 to view all expenses")
-    print("Enter 3 to exit")
-    choice = input("Enter your choice: ")
 
-    if choice == "1":
-        date = input("Enter the date of the expense (YYYY-MM-DD): ")
-        category = input("Enter the category of the expense: ")
-        amount = float(input("Enter the amount of the expense: "))
-        description = input("Enter a description of the expense: ")
-        database.add_expense(date, category, amount, description)
+@app.command()
+def show():
+    database.view_expenses()
 
-    elif choice == "2":
-        database.view_expenses()
 
-    elif choice == "3":
-        break
-
-    else:
-        print("Invalid choice. Try again.")
-
-database.close()
+if __name__ == "__main__":
+    app()

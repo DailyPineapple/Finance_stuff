02a415e84c2be896190d823157b32169840edd0c
DailyPineapple <72655128+DailyPineapple@users.noreply.github.com>
2023-09-26 22:21:09 +0200

    New Typer

---
+++ main.py
@@ -0,0 +1,42 @@
+import typer
+from src.db import Database
+
+app = typer.Typer()
+
+@app.command()
+def create_tables():
+    db = Database("finance_tracker.db")
+    db.create_tables()
+    db.close()
+    typer.echo("Tables created successfully.")
+
+@app.command()
+def add_expense(
+    expense_info: str = typer.Option(..., help="Enter the expense details in the format: Date,Category,Amount,Description"),
+):
+    parts = expense_info.split(',')
+    if len(parts) != 4:
+        typer.echo("Invalid input format. Please provide Date, Category, Amount, and Description separated by commas.")
+        return
+
+    date, category, amount, description = [part.strip() for part in parts]
+        
+    try:
+        amount = float(amount)
+    except ValueError:
+        typer.echo("Invalid amount format. Amount should be a number.")
+        return
+
+    db = Database("finance_tracker.db")
+    db.add_expense(date, category, amount, description)
+    db.close()
+    typer.echo("Expense added successfully.")
+
+@app.command()
+def view_expenses():
+    db = Database("finance_tracker.db")
+    db.view_expenses()
+    db.close()
+
+if __name__ == "__main__":
+    app()
